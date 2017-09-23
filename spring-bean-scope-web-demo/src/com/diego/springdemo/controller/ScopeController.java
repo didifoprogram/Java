98820ba666b5f064f5766aee986cfb39bbd91210
Diego <didifoprogram@gmail.com>
2017-09-23 17:48:36 -0300

    adds bean-scope project

---
+++ spring-bean-scope-web-demo/src/com/diego/springdemo/controller/ScopeController.java
@@ -0,0 +1,32 @@
+package com.diego.springdemo.controller;
+
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.diego.springdemo.service.RandomizerRequestScope;
+import com.diego.springdemo.service.RandomizerSessionScope;
+
+@Controller
+public class ScopeController {
+	
+	@Autowired
+	RandomizerRequestScope randomizerRequestScope;
+	
+	@Autowired
+	RandomizerSessionScope randomizerSessionScope;
+	
+	@RequestMapping("/")
+	public void getRandom(HttpServletResponse httpServletResponse) throws Exception {
+		httpServletResponse.getWriter().write("Request scope <br>");
+		httpServletResponse.getWriter().write("Old number " + randomizerRequestScope.getRandomNumber() + " <br>");
+		randomizerRequestScope.generateRandomNumber();
+		httpServletResponse.getWriter().write("New Number " + randomizerRequestScope.getRandomNumber() + " <br>");
+		
+		httpServletResponse.getWriter().write("Session Scope <br>");
+		httpServletResponse.getWriter().write("Old number " + randomizerSessionScope.getRandomNumber() + " <br>");
+		randomizerSessionScope.generateRandomNumber();
+		httpServletResponse.getWriter().write("New number : " + randomizerSessionScope.getRandomNumber() + " <br>");
+	}
+}
---
+++ spring-bean-scope-web-demo/src/com/diego/springdemo/service/RandomizerSessionScope.java
@@ -0,0 +1,13 @@
+package com.diego.springdemo.service;
+
+public class RandomizerSessionScope {
+	private int randomNumber = 10;
+	
+	public int getRandomNumber() {
+		return this.randomNumber;
+	}
+	
+	public void generateRandomNumber() {
+		this.randomNumber = (int) (Math.random() * 99999);
+	}
+}
