6d669be92622b2bcd6706ea9393e1e6b4943a51f
Diego <didifoprogram@gmail.com>
2017-09-23 12:40:23 -0300

    added javax.servlet dependency

--- spring-bean-scope-web-demo/src/com/diego/springdemo/controller/ScopeController.java
+++ spring-bean-scope-web-demo/src/com/diego/springdemo/controller/ScopeController.java
@@ -1,7 +1,6 @@
 package com.diego.springdemo.controller;
 
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
