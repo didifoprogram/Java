4202e60714ea9360f29083d70dc1a7ed8d48122b
diego <didifoprogram@gmail.com>
2018-04-17 15:05:11 -0300

    add positional list

---
+++ untitled/src/com/company/LinkedPositionalList.java
@@ -0,0 +1,152 @@
+package com.company;
+
+public class LinkedPositionalList<E> implements PositionalList<E> {
+    //---------------nested Node class-----------------
+    private static class Node<E> implements Position<E> {
+        private E element;
+        private Node<E> prev;
+        private Node<E> next;
+        public Node(E e, Node<E> p, Node<E> n) {
+            element = e;
+            prev = p;
+            next = n;
+        }
+
+        @Override
+        public E getElement() throws IllegalStateException {
+            if (next == null)
+                throw new IllegalStateException("Position no longer valid");
+            return element;
+        }
+
+        public Node<E> getPrev() {
+            return prev;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setElement(E e) {
+            element = e;
+        }
+
+        public void setPrev(Node<E> p) {
+            prev = p;
+        }
+
+        public void setNext(Node<E> n) {
+            next = n;
+        }
+    } //---------------end of nested class------------------
+
+    private Node<E> header;
+    private Node<E> trailer;
+    private int size = 0;
+
+
+    //** Constructs a new empty list
+    public LinkedPositionalList() {
+        header = new Node<>(null, null, null);
+        trailer = new Node<>(null, header, null);
+        header.setNext(trailer);
+    }
+
+    // private utilities
+    // ** Validates the position and return it as a node
+    private Node<E> validate(Position<E> p) throws IllegalArgumentException {
+        if (!(p instanceof Node)) throw new IllegalArgumentException("Invalid p");
+        Node<E> node = (Node<E>) p; // safe cast;
+        if (node.getNext() == null)
+            throw new IllegalArgumentException("p is no longer in the list");
+        return node;
+    }
+
+    //** Returns the given node as a Position (or null if it is a sentinel).
+    private Position<E> position(Node<E> node) {
+        if (node == header || node == trailer)
+            return null; // do not expose user to sentinels
+        return node;
+    }
+
+    // accessors
+    public int size() { return size; }
+
+    public boolean isEmpty() { return size == 0; }
+
+    public Position<E> first() {
+        return position(header.getNext());
+    }
+
+    public Position<E> last() {
+        return position(trailer.getPrev());
+    }
+
+    public Position<E> before(Position<E> p) throws IllegalArgumentException {
+        Node<E> node = validate(p);
+        return position(node.getPrev());
+    }
+
+    public Position<E> after(Position<E> p) throws IllegalArgumentException {
+        Node<E> node = validate(p);
+        return position(node.getNext());
+    }
+
+    private Position<E> addBetween(E e, Node<E> pred, Node<E> succ) {
+        Node<E> newest = new Node<>(e, pred, succ);
+        pred.setNext(newest);
+        succ.setPrev(newest);
+        size++;
+        return newest;
+    }
+
+    public Position<E> addFirst(E e) {
+        return addBetween(e, header, header.getNext());
+    }
+
+    public Position<E> addLast(E e) {
+        return addBetween(e, trailer.getPrev(), trailer);
+    }
+
+    public Position<E> addBefore(Position<E> p, E e) throws IllegalArgumentException {
+        Node<E> node = validate(p);
+        return addBetween(e, node.getPrev(), node);
+    }
+
+    public Position<E> addAfter(Position<E> p, E e) throws IllegalArgumentException {
+        Node<E> node = validate(p);
+        return addBetween(e, node, node.getNext());
+    }
+
+    // Replace element at Position p
+    public E set(Position<E> p, E e) throws IllegalArgumentException {
+        Node<E> node = validate(p);
+        E answer = node.getElement();
+        node.setElement(e);
+        return answer;
+    }
+
+    // Removes the element at Position p
+    public E remove(Position<E> p) throws IllegalArgumentException {
+        Node<E> node = validate(p);
+        Node<E> pred = node.getPrev();
+        Node<E> succ = node.getNext();
+        pred.setNext(succ);
+        succ.setPrev(pred);
+        size--;
+        E answer = node.getElement();
+        node.setElement(null); // help with garbage collection
+        node.setNext(null);
+        node.setPrev(null);
+        return answer;
+    }
+
+
+
+
+
+
+
+
+
+}
